f5716b4a2a04707d43f70fe2600a1c8e1eac4eb4
Alaa Anwer <anwaralaa286@gmail.com>
2026-03-12 09:59:59 +0200

    update user routes

---
+++ routes/web.php
@@ -0,0 +1,57 @@
+<?php
+require_once __DIR__ . '/../app/config/Database.php';
+require_once __DIR__ . '/../app/controllers/HomeController.php';
+require_once __DIR__ . '/../app/controllers/OrderController.php';
+function handle_cart_action(OrderController $orderController): void
+{
+    if (empty($_GET['action']) || empty($_GET['id'])) {
+        return;
+    }
+    // index.php?action=add&id=3   =>  action = add, id = 3
+    $action = $_GET['action'];
+    $id     = (int) $_GET['id'];
+
+    switch ($action) {
+        case 'add':
+            $orderController->add($id);
+            break;
+
+        case 'plus':
+            $orderController->increase($id);
+            break;
+
+        case 'minus':
+            $orderController->decrease($id);
+            break;
+
+        default:
+            return;
+    }
+    // POST/Redirect/GET Pattern
+    header("Location: index.php");
+    exit();
+}
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+$action = $_GET['action'] ?? 'home';
+
+switch ($action) {
+    case 'add':
+    case 'plus':
+    case 'minus':
+        $db   = new Database();
+        $conn = $db->connect();
+        $orderController = new OrderController($conn);
+        handle_cart_action($orderController);
+        break;
+
+    case 'home':
+    default:
+        $home = new HomeController();
+        $home->index();
+        break;
+}
+
